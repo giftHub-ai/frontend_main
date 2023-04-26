a0dc249a0e3b4b94caee71df92ce9357e3dabf98
Vinit Kumbhare <studyvinit99@gmail.com>
2023-04-26 15:03:02 +0530

    removed dark mode tailwind css

--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -10,13 +10,13 @@ const InputComponent = ({
   return (
     <div className="w-full flex flex-col sm:flex-row sm:items-center sm:justify-center ">
       <label
-        className="mb-2 sm:mx-4 text-sm font-medium text-gray-900 dark:text-white"
+        className="mb-2 sm:mx-4 text-sm font-medium text-gray-900"
         htmlFor={labelId}
       >
         {labelName}
       </label>
       <input
-        className="w-full p-2 sm:mx-4 bg-gray-50 border border-gray-300 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="w-full p-2 sm:mx-4 bg-gray-50 border border-gray-300 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500"
         type={type}
         placeholder={placeholder}
         id="age"
