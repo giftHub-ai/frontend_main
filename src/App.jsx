17644ec551603e7e8daf9f37f40b2cf7897344fc
Vinit Kumbhare <studyvinit99@gmail.com>
2023-04-26 18:30:51 +0530

    merged origin/master

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import InputComponent from "./components/InputComponent";
 import MultiStepForm from "./components/MultiStepForm";
-import SubmitButton from "./components/Button";
 import "./index.css";
-import { useForm } from "react-hook-form";
 
 function App() {
-  const { register, handleSubmit } = useForm();
-
-  const handleFormSubmit = (formData) => {
-    console.log(formData);
-  };
-
   return (
     <div className="flex justify-between items-center h-screen">
       <div className=" max-w-xl mx-auto p-4">
-        <MultiStepForm/>
+        <MultiStepForm />
       </div>
     </div>
   );
