d044f8a779775fdceb442db8458e953ce218fbe9
Twinshu <twinshup@gmail.com>
2023-04-30 23:17:31 +0530

    heading added context api

--- src/components/formSteps/GenderInputStep.jsx
+++ src/components/formSteps/GenderInputStep.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import female from "../../assets/Female.png";
 import male from "../../assets/Male.png";
-import Button from "../Button";
 import PrevNext from "../PrevNext";
+import { UserContext, UserDispatchContext } from "../Context";
 
 const GenderInputStep = () => {
+  const inputDetails = React.useContext(UserContext);
+  const setUserDetails = useContext(UserDispatchContext);
   return (
     <div className="">
+       <h1 className={inputDetails.headingStyle}>
+        Pick Recipient's Gender
+      </h1>
       <div className="w-full flex justify-evenly ">
         <div className=" p-2 m-2">
           <img src={male} alt="male"></img>
@@ -15,7 +20,7 @@ const GenderInputStep = () => {
           <img src={female} alt="female"></img>
         </div>
       </div>
-  <PrevNext/>
+      <PrevNext />
     </div>
   );
 };
